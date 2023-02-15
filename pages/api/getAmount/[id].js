9756e80ad1ddb8bc5cc2823a06cb53e9f877ccfe
Naveen <87268854+iam-naveen@users.noreply.github.com>
2023-02-15 17:46:32 +0530

    fix for chrome not starting in the server

--- pages/api/getAmount/[id].js
+++ pages/api/getAmount/[id].js
@@ -1,50 +1,62 @@
-import puppeteer from "puppeteer";
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 
 export default function handler(req, res) {
   const { query } = req;
   const { id } = query;
 
   (async () => {
-    return new Promise(async (resolve) => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-
-      await page.goto("http://www.wallet.sbowork.org.in/");
-
-      // Type into search box
-      await page
-        .type("#wrapper > div > form > label > input", `${id}`)
-        .then(() => page.keyboard.press("Enter"));
-
-      // Locate the elements
-      const workingWalletElement =
-        "#wrapper > div > div.walletbox > ul > li:nth-child(2) > h3";
-      const nonWorkingWalletElement =
-        "#wrapper > div > div.walletbox > ul > li:nth-child(1) > h3";
-
-      // Get the content needed
-      const textSelector1 = await page.waitForSelector(workingWalletElement);
-      const workingWallet = await textSelector1.evaluate(
-        (el) => el.textContent
-      );
-
-      const textSelector2 = await page.waitForSelector(nonWorkingWalletElement);
-      const nonWorkingWallet = await textSelector2.evaluate(
-        (el) => el.textContent
-      );
-
-      const response = {
-        workingWallet: `${workingWallet}`,
-        nonWorkingWallet: `${nonWorkingWallet}`,
+    let options = {}
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      options = {
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTSErrors: true,
       };
+    }
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
 
-      // close the instance
-      browser.close();
+    await page.goto("http://www.wallet.sbowork.org.in/");
 
-      // return the response
-      res.status(200).json(response);
+    // Type into search box
+    await page
+      .type("#wrapper > div > form > label > input", `${id}`)
+      .then(() => page.keyboard.press("Enter"));
 
-      return resolve();
-    });
+    // Locate the elements
+    const workingWalletElement =
+      "#wrapper > div > div.walletbox > ul > li:nth-child(2) > h3";
+    const nonWorkingWalletElement =
+      "#wrapper > div > div.walletbox > ul > li:nth-child(1) > h3";
+
+    // Get the content needed
+    const textSelector1 = await page.waitForSelector(workingWalletElement);
+    const workingWallet = await textSelector1.evaluate((el) => el.textContent);
+
+    const textSelector2 = await page.waitForSelector(nonWorkingWalletElement);
+    const nonWorkingWallet = await textSelector2.evaluate(
+      (el) => el.textContent
+    );
+
+    const response = {
+      workingWallet: `${workingWallet}`,
+      nonWorkingWallet: `${nonWorkingWallet}`,
+    };
+
+    // close the instance
+    browser.close();
+
+    // return the response
+    res.status(200).json(response);
   })();
 }
